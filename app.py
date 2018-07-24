50fe9cb7661a944dcda7d1691f462500be4728eb
Mohamed K <m.kore@mimicoweb.com>
2018-07-24 10:33:51 -0400

    added cors to api

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask_restful import Api
+
 from resources import KijijiAds
 
 from db import app
 api = Api(app)
 
 api.add_resource(KijijiAds, '/car-api/make=<string:make>&model=<string:model>')
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
