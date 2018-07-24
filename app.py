fe8b2c0d00b8e333da7e772edb72237061810978
Mohamed K <m.kore@mimicoweb.com>
2018-07-23 23:32:40 -0400

    changed root of api route

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from resources import KijijiAds
 from db import app
 api = Api(app)
 
-api.add_resource(KijijiAds, '/make=<string:make>&model=<string:model>')
+api.add_resource(KijijiAds, '/car-api/make=<string:make>&model=<string:model>')
