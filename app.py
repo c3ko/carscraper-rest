1b79faea76da73c357e827b3dde60e7cfbde9314
Mohamed K <m.kore@mimicoweb.com>
2018-11-25 12:05:14 -0500

    added requirements freeze

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ api.add_resource(SavedCars, '/saved-car/<int:save_id>')
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
+
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   return response
